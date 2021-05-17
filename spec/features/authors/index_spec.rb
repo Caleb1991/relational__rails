00c64093637812b4a3977c3006e01be71030474d
ahenzler <aliciahenzler@outlook.com>
2021-05-17 12:18:10 -0600

    add links

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'Authors Index Page' do
     @author_1 = Author.create!(name: 'C.S. Lewis', alive: false, age: 64)
     @author_2 = Author.create!(name: 'Leigh Bardugo', alive: true, age: 46)
 
-    @author_1.books.create!(title: 'Narnia', series: true, page_count: 237)
-    @author_1.books.create!(title: 'Prince Caspian', series: true, page_count: 237)
-    @author_2.books.create!(title: 'Shadow and Bone', series: true, page_count: 237)
-    @author_2.books.create!(title: 'Siege and Storm', series: true, page_count: 357)
+    @book_1 = @author_1.books.create!(title: 'Narnia', series: true, page_count: 237)
+    @book_2 = @author_1.books.create!(title: 'Prince Caspian', series: true, page_count: 237)
+    @book_3 = @author_2.books.create!(title: 'Shadow and Bone', series: true, page_count: 237)
+    @book_4 = @author_2.books.create!(title: 'Siege and Storm', series: true, page_count: 357)
 
     visit "/authors"
   end
--- spec/features/authors/show_authors_with_books.rb
+++ spec/features/authors/show_authors_with_books.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+RSpec.describe 'Authors Index Page' do
+  before :each do
+    @author_1 = Author.create!(name: 'C.S. Lewis', alive: false, age: 64)
+    @author_2 = Author.create!(name: 'Leigh Bardugo', alive: true, age: 46)
+
+    @book_1 = @author_1.books.create!(title: 'Narnia', series: true, page_count: 237)
+    @book_2 = @author_1.books.create!(title: 'Prince Caspian', series: true, page_count: 237)
+    @book_3 = @author_2.books.create!(title: 'Shadow and Bone', series: true, page_count: 237)
+    @book_4 = @author_2.books.create!(title: 'Siege and Storm', series: true, page_count: 357)
+
+    visit "/authors"
+  end
+end
