feac05baf717f76aac0bd2a38798901afc502914
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-18 08:12:07 -0600

    Creat update game functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,24 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.all.where(physical_release: true)
   end
 
   def show
     @game = Game.find(params[:id])
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.update(game_params)
+    redirect_to "/games/#{game.id}"
+  end
+
+  private
+  def game_params
+    params.permit(:name, :year_released, :consoles, :physical_release, :esrb_rating)
   end
 end
