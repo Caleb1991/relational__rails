54bc03d17d2a09c8f8ed9fa81b45fe13bd59b342
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-19 13:25:44 -0600

    Create functionaly for user integer input to narrow results field

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -8,4 +8,14 @@ class Developer < ApplicationRecord
   def self.order_by_created
     all.order(created_at: :desc)
   end
+
+  def released_after(user_number)
+    integer = user_number.to_i
+
+    games.where('year_released > ?', integer)
+  end
+
+  def order_by_name
+    games.order(:name)
+  end
 end
