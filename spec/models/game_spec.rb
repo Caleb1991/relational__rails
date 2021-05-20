fff618aa275127c65f4c6b6f37e869937e65f2d3
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-19 19:41:17 -0600

    Create model tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Game, type: :model do
   describe 'relationships' do
     it { should belong_to(:developer)}
   end
+
+  describe 'initialize' do
+    it 'exists' do
+      @developer_1 = Developer.create!(name: 'Bethesda', year_founded: 1969, still_active: true, city: 'Bethesda', number_of_staff: 420)
+      @game_1 = @developer_1.games.create!(name: 'Skyrim', year_released: 2010, consoles: 'PS4', physical_release: true, esrb_rating: 'M')
+
+      expect(@game_1).to be_an_instance_of(Game)
+    end
+  end
 end
